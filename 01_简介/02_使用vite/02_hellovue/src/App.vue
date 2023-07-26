882d3a5123502b1aac84494ec58ae28c789fd6f7
Mli-ZT <mli.bio@outlook.com>
2023-07-26 10:09:20 +0800

    node v18.17.0, yarn v1.22.19

---
+++ 01_简介/02_使用vite/02_hellovue/src/App.vue
@@ -0,0 +1,20 @@
+<script>
+    import MyButton from "./components/MyButton.vue";
+    // 编写组件代码
+    export default {
+        data() {
+            return {
+                msg: "VUE 学习",
+            };
+        },
+
+        components: {
+            MyButton,
+        },
+    };
+</script>
+
+<template>
+    <h1>{{ msg }}</h1>
+    <MyButton></MyButton>
+</template>
---
+++ 02_vue基础/src/App.vue
@@ -0,0 +1,23 @@
+<script>
+// 组件是一个普通的 js 对象
+export default {
+    // data 是一个函数
+    // 在 data 中，this 就是当前的组件实例 vm
+    // 如果使用箭头函数，则无法通过 this 来访问组件实例
+    // 使用 vue 时，减少使用箭头函数
+    data() {
+        // console.log("data", this)
+        // 直接向组件实例中添加一个属性
+        this.name = "孙悟空"
+        // data 会返回一个对象作为返回值，vue 会对对象进行代理，将其转换为响应式数据，响应式数据可以直接通过组件实例访问
+        return {
+            msg: "Vue 基础"
+        }
+    }
+}
+</script>
+
+<template>
+    <h1>{{ msg }}</h1>
+    <h2>{{ name }}</h2>
+</template>
