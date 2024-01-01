0cf068e8d6b1d07f9b2042b1740d10657eeea97d
Mli-ZT <mli.bio@outlook.com>
2024-01-01 22:39:15 +0800

    1/1/2024 update

---
+++ 03_vue基础/src/04_组合式API简介.vue
@@ -0,0 +1,42 @@
+<script>
+
+import { reactive, readonly } from 'vue'
+
+export default {
+    // setup 是一个钩子函数，可以通过这个函数向外部暴露组件的配置
+    setup() {
+        // 定义变量
+        // 在组合式 API 中直接声明的变量就是一个普通变量，不是响应式变量，修改属性时不会再视图中产生效果
+        let msg = "今天天气真不错！"
+        let count = 0
+
+        // 可以通过 reactive() 来创建一个响应式对象
+        const stu = reactive({
+            name: "孙悟空",
+            age: 18,
+            gender: "男"
+        })
+
+        function changeAge() {
+            stu.age = 44
+        }
+
+        // 在 setup() 中可以通过返回值来指定哪些内容要暴露给外部
+        // 暴露后的内容可以在模板中直接使用
+        return {
+            msg,
+            count,
+            stu,
+            changeAge
+        }
+    }
+}
+</script>
+
+<template>
+    <h1>演示组合式 API</h1>
+    <h2>{{ msg }}</h2>
+    <h3>{{ count }}</h3>
+    <button @click="changeAge">点我一下</button>
+    <h2>{{ stu.name }} -- {{ stu.age }} -- {{ stu.gender }}</h2>
+</template>
---
+++ 04_模板/src/App.vue
@@ -0,0 +1,43 @@
+<script setup>
+
+import { ref, reactive, computed } from 'vue'
+
+const msg = ref("Hello Vue")
+
+// {value: obj}
+// obj.value.name
+const obj = ref({
+    name: "孙悟空",
+    age: 18
+})
+
+// obj2.name.value
+const obj2 = {
+    name: ref("孙悟空"), // {value: "孙悟空"}
+    age: ref(18)
+}
+
+const changeMsgHandler = () => {
+    // 修改 ref 对象时，必须通过 value
+    msg.value = "66"
+}
+
+// computed 用来生成计算属性
+const newMsg = computed(() => {
+    return msg.value + "-我爱 Vue"
+})
+
+</script>
+
+<template>
+    <!-- ref 对象在模板中可以自动解包（要求 ref 对象必须是顶层对象） -->
+    <h1>{{ msg }}</h1>
+    <h1>{{ newMsg }}</h1>
+    <h2>{{ obj.name }}</h2>
+    <!-- name 不是顶层响应式对象，所以不能自动解包 -->
+    <h2>{{ obj2.name.value }}</h2>
+    <hr>
+    <h2>{{ obj.age }}</h2>
+    <h2>{{ obj2.age }}</h2>
+    <button @click="changeMsgHandler">点我一下</button>
+</template>
